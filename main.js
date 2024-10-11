57afb43236be609c8c8aed2748351ff01898e50d
Daniel <125220325+WebfitDesign@users.noreply.github.com>
2024-10-11 09:30:11 +0100

    finished dynamic temp

---
+++ main.js
@@ -0,0 +1,9 @@
+async function start() {
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/TOP/32,81/forecast")
+  const weatherData = await weatherPromise.json()
+  const ourTemperature = weatherData.properties.periods[0].temperature
+
+  document.querySelector("#temperature-output").textContent = ourTemperature
+}
+
+start()
